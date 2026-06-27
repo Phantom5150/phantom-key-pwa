c69d5df382f1a9b49cfbb7b15458436bdce93e03
Mike Shaffer <info@phantomkeyecu.com>
2026-06-27 17:35:49 -0400

    cleanup fix script

--- fix.ps1
+++
@@ -1,7 +0,0 @@
-$f = 'C:\PhantomKey\PWA\index.html'
-$c = [System.IO.File]::ReadAllText($f)
-$i = $c.IndexOf('<div id="root">')
-$e = $c.IndexOf('</div>', $i) + 6
-$c = $c.Substring(0, $i) + '<div id="root"></div>' + $c.Substring($e)
-[System.IO.File]::WriteAllText($f, $c)
-Write-Host "Done"
